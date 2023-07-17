900d533457a3762e0b3c307292a5f050c5a9ce0e
Matthew Tartt <87999009+Tartticus@users.noreply.github.com>
2023-07-16 22:26:56 -0700

    Update date recommender.py

--- date recommender.py
+++ date recommender.py
@@ -1,3 +1,4 @@
+'''
 # -*- coding: utf-8 -*-
 """
 Created on Tue Jun 13 19:47:41 2023
@@ -23,7 +24,7 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import accuracy_score, classification_report
 print("imports complete")
-
+'''
 
 #%% Cursor connect
 hitlist = mysql.connector.connect(
