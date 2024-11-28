6c41fcaac2283229534cc9ee89704043bbcd4643
Matthew Tartt <87999009+Tartticus@users.noreply.github.com>
2024-11-27 21:38:16 -0800

    Create databasecreation.py

---
+++ Database creation/Duckdb/databasecreation.py
@@ -0,0 +1,28 @@
+import duckdb
+
+# Connect to an in-memory DuckDB database
+con = duckdb.connect(database='dining')
+
+# Create a table
+con.execute("""
+CREATE TABLE users (
+    user_id INT PRIMARY KEY,
+    date_created DATETIME,
+    username VARCHAR,
+    email VARCHAR
+);
+"""
+)
+
+
+con.execute("""
+            CREATE TABLE Restaurants (
+    RestaurantID SERIAL PRIMARY KEY, -- Unique identifier for each record
+    CreatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- Timestamp for record creation
+    username VARCHAR (50),
+    PlaceID VARCHAR(50) UNIQUE NOT NULL, -- Place ID from the API
+    Resturaant_Name VARCHAR(100) NOT NULL, -- Name of the restaurant
+    City VARCHAR(100), -- City name for filtering
+    URL TEXT, -- Website URL
+    Rating FLOAT, -- Average rating    
+);""")
